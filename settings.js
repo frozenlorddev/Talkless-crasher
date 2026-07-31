9f1a0994cb25384899cf722f41a438a9e3bdbada
frozenlorddev <frozenlord254@gmail.com>
2026-07-31 10:30:38 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,25 @@
+
+/**
+* adevos
+* adevos
+*/
+
+const settings = {
+  packname: '𝐁𝐋𝐀𝐂𝐊𝐋𝐎𝐑𝐃 𝐈𝐍𝐂 ',
+  author: '𝐁𝐋𝐀𝐂𝐊𝐋𝐎𝐑𝐃 𝐈𝐍𝐂 ',
+  botName: "𝐁𝐋𝐀𝐂𝐊𝐋𝐎𝐑𝐃 𝐈𝐍𝐂 ",
+  botOwner: '𝐁𝐋𝐀𝐂𝐊𝐋𝐎𝐑𝐃 𝐈𝐍𝐂 ', // Your name
+  ownerNumber: process.env.OWNER_NUMBER || '255675421210', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.0",
+  updateBranch: 'main',
+  updateZipToken: '',
+  updateZipUrl: "https://github.com/adevos-x-tech/adevosxbot/archive/refs/heads/main.zip",
+  timezone: 'Africa/nairobi',
+};
+
+module.exports = settings;
